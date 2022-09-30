e92acbf4392ab3ee044dcc24dc51324f432dae83
timbaron <timothy.akiode@student.aul.edu.ng>
2022-09-30 07:07:22 +0100

    redesign input

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 export default function Form() {
   return (
-    <div className="flex justify-center mt-5">
+    <div className="flex justify-center mt-9">
       <input type="text" className="border border-gray-400 p-2 rounded mr-3" />
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      <button className="button">
         Add
       </button>
     </div>
